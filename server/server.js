5a5f7b1182b10a1ae46f63744011434e49f5e6e0
kelseywarren <warrenkelsey@outlook.com>
2024-05-27 09:23:52 -0500

    Send json data to localhost

--- server/server.js
+++ server/server.js
@@ -4,15 +4,16 @@ const axios = require('axios');
 const port = 5500;
 const apiKey = "b389ed0c-d634-4243-841d-f38788db5e9c";
 
-app.get("/api", (request, response) => {
+app.get("/api", (req, res) => {
     axios
-      .get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?limit=10", {
+      .get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10", {
         headers: {
           "X-CMC_PRO_API_KEY": `${apiKey}`,
         },
       })
       .then((response) => {
         console.log(response.data);
+        res.json(response.data) // sends json data to localhost
       })
       .catch((error) => {
         console.log(error);
