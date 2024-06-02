6fa178096e01209defa3f41c2c8a71851d8b6c4e
kelseywarren <warrenkelsey@outlook.com>
2024-06-02 16:56:07 -0500

    Remove info request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import './App.css'
 function App() {
 
   const [backendData, setBackendData] = useState([]);
-  const [info, setInfo] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5500/api")
@@ -15,29 +14,17 @@ function App() {
     })
   }, [])
 
-  useEffect(() => {
-    fetch("http://localhost:5500/info")
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.BTC[0].logo)
-      setInfo(data.BTC[0])
-    })
-  }, [])
-
   return (
     <div>
-      <img src={info.logo}></img>
       {backendData.map((crypto, index) => {
         return (
           <div>
-            <p key={index}>{crypto.name}</p>
-            <p key={index}>{crypto.symbol}</p>
-            <p key={index}>{crypto.quote.USD.price}</p>
-            <p key={index}>{crypto.cmc_rank}</p>
+            <span><p key={index}>{crypto.cmc_rank} {crypto.name} {crypto.symbol} ${crypto.quote.USD.price}</p></span>
           </div>
         )
       })}
     </div>
+    
   )
 }
 
